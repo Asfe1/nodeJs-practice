db7f79efbcdc87c4b4f7edf8dced77cb9fd2021b
Asfe1 <aarafath172154@bscse.uiu.ac.bd>
2022-04-21 03:41:51 +0600

    noed practice done

---
+++ index.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('hey look here. I can run express')
+});
+const users = [
+    {
+        "id": 1,
+        "name": "Leanne Graham",
+        "username": "Bret",
+        "email": "Sincere@april.biz",
+        "address": {
+            "street": "Kulas Light",
+            "suite": "Apt. 556",
+            "city": "Gwenborough",
+            "zipcode": "92998-3874",
+            "geo": {
+                "lat": "-37.3159",
+                "lng": "81.1496"
+            }
+        },
+        "phone": "1-770-736-8031 x56442",
+        "website": "hildegard.org",
+        "company": {
+            "name": "Romaguera-Crona",
+            "catchPhrase": "Multi-layered client-server neural-net",
+            "bs": "harness real-time e-markets"
+        }
+    },
+    {
+        "id": 2,
+        "name": "Ervin Howell",
+        "username": "Antonette",
+        "email": "Shanna@melissa.tv",
+        "address": {
+            "street": "Victor Plains",
+            "suite": "Suite 879",
+            "city": "Wisokyburgh",
+            "zipcode": "90566-7771",
+            "geo": {
+                "lat": "-43.9509",
+                "lng": "-34.4618"
+            }
+        }
+    }];
+
+app.get('/users', (req, res) => {
+    if (req.query.name) {
+        const search = req.query.name.toLowerCase();
+        const matched = users.filter(user => user.name.toLowerCase().includes(search))
+        res.send(matched)
+    }
+    else {
+        res.send(users)
+    }
+
+    console.log('query', req.query);
+});
+app.get('/user/:id', (req, res) => {
+    console.log(req.params)
+    const id = req.params.id;
+
+    // const user = users[parseInt(id) - 1];
+    const user = users.find(u => u.id == id);
+    res.send(user)
+});
+
+app.post('/user', (req, res) => {
+    const user = req.body;
+    user.id = users.length + 1;
+    users.push(user);
+    res.send(user)
+})
+app.listen(port, () => {
+    console.log('Listebing to port', port);
+})
